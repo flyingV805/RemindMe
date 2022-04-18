108b9bb175d0637d0bd59bea6c322abc7c884043
FlyingV <petrov.vladislav1992@gmail.com>
2022-04-19 00:06:31 +0600

    Sync

--- app/src/main/java/kz/flyingv/remindme/activity/main/MainActivity.kt
+++ app/src/main/java/kz/flyingv/remindme/activity/main/MainActivity.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
-import kz.flyingv.remindme.R
 import kz.flyingv.remindme.activity.main.action.MainAction
 import kz.flyingv.remindme.activity.main.fragment.NewReminderDialog
 import kz.flyingv.remindme.activity.main.fragment.NewReminderViewModel
@@ -171,120 +170,6 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    private val iconList = listOf(R.drawable.ic_avatar_cake, R.drawable.ic_avatar_medeicine, R.drawable.ic_avatar_officials, R.drawable.ic_avatar_payday, R.drawable.ic_avatar_workout)
-
-    /*@OptIn(ExperimentalMaterialApi::class)
-    @Composable
-    private fun NewReminderDialog(dialogState: ModalBottomSheetState){
-
-        val scope = rememberCoroutineScope()
-
-        val remindTypes = remember { listOf(RemindTypeEnum.Daily, RemindTypeEnum.Weekly, RemindTypeEnum.Monthly, RemindTypeEnum.Yearly) }
-        var selectedRemindType by remember { mutableStateOf(RemindTypeEnum.Daily) }
-        var selectIcon by remember { mutableStateOf(0) }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalAlignment = CenterHorizontally
-        ) {
-            Text(text ="NEW REMINDER", style = typography.h6)
-            Spacer(modifier = Modifier.height(16.dp))
-            IconSelector(
-                modifier = Modifier.fillMaxWidth(),
-                icons = iconList,
-                onSelectionChanged = {selectIconIndex -> selectIcon = selectIconIndex}
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = "",
-                singleLine = true,
-                onValueChange = {},
-                placeholder = { Text("Reminder Name") },
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            SegmentedControl(
-                remindTypes,
-                selectedRemindType,
-                onSegmentSelected = { selectedRemindType = it }
-            ) {
-                SegmentText(
-                    when(it){
-                        RemindTypeEnum.Daily -> "Daily"
-                        RemindTypeEnum.Weekly -> "Weekly"
-                        RemindTypeEnum.Monthly -> "Monthly"
-                        RemindTypeEnum.Yearly -> "Yearly"
-                    }
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(64.dp),
-                contentAlignment = Center
-            ) {
-                androidx.compose.animation.AnimatedVisibility(
-                    enter = fadeIn(), exit = fadeOut(),
-                    visible = selectedRemindType == RemindTypeEnum.Daily
-                ) {
-                    Text("Remind every day")
-                }
-                androidx.compose.animation.AnimatedVisibility(
-                    enter = fadeIn(), exit = fadeOut(),
-                    visible = selectedRemindType == RemindTypeEnum.Weekly
-                ) {
-                    DayOfWeekSelector(
-                        modifier = Modifier.fillMaxWidth(),
-                        onSelectionChanged = {}
-                    )
-                }
-                androidx.compose.animation.AnimatedVisibility(
-                    enter = fadeIn(), exit = fadeOut(),
-                    visible = selectedRemindType == RemindTypeEnum.Monthly
-                ) {
-                    DayOfMonthSelector(
-                        modifier = Modifier.fillMaxWidth(),
-                        onSelectionChanged = {}
-                    )
-                }
-                androidx.compose.animation.AnimatedVisibility(
-                    enter = fadeIn(), exit = fadeOut(),
-                    visible = selectedRemindType == RemindTypeEnum.Yearly
-                ) {
-                    DayOfYearSelector(
-                        modifier = Modifier.fillMaxWidth(),
-                        onSelectionChanged = {day, month ->  }
-                    )
-                }
-            }
-
-            Text(text ="VIEW DETAIL", style = typography.h6)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "VIEW DETAIL", style = typography.caption)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text ="VIEW DETAIL", style = typography.h6)
-            Text(text = "VIEW DETAIL", style = typography.caption)
-            Text(text ="VIEW DETAIL", style = typography.h6)
-            Text(text = "VIEW DETAIL", style = typography.caption)
-            Spacer(modifier = Modifier.height(16.dp))
-            ExtendedFloatingActionButton(
-                icon = { Icon(Icons.Filled.Create,"") },
-                text = { Text("CREATE REMINDER") },
-                elevation = FloatingActionButtonDefaults.elevation(8.dp),
-                onClick = {
-                    scope.launch { dialogState.hide() }
-                    viewModel.createReminder()
-                }
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-        }
-    }*/
 
     @Composable
     private fun ReminderList(modifier: Modifier, reminders: List<Reminder>){
@@ -332,7 +217,7 @@ class MainActivity : ComponentActivity() {
                 }
                 Spacer(modifier = Modifier.width(8.dp))
                 Column(Modifier.weight(1f)) {
-                    Text(text = "Grandmas birthday", style = typography.h6)
+                    Text(text = reminder.name, style = typography.h6)
                     Text(text = "Every month, 6-th", style = typography.h6)
                     Spacer(modifier = Modifier.height(4.dp))
                     Text(text = "Last remind: 2 days ago", style = typography.caption)
@@ -342,91 +227,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-/*
-    @Composable
-    fun IconSelector(){
-        var selectIcon by remember { mutableStateOf(Icons.Filled.ThumbUp) }
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ){
-            Box(modifier = Modifier
-                .shadow(4.dp, CircleShape)
-                .background(
-                    if (selectIcon == Icons.Filled.ThumbUp) {
-                        Color.Cyan
-                    } else {
-                        Color.White
-                    }, CircleShape
-                )
-                .clickable { selectIcon = Icons.Filled.ThumbUp }){ Icon(Icons.Filled.ThumbUp, "",
-                Modifier
-                    .padding(12.dp)
-                    .width(24.dp)
-                    .height(24.dp)) }
-            Spacer(modifier = Modifier.width(8.dp))
-            Box(modifier = Modifier
-                .shadow(4.dp, CircleShape)
-                .background(
-                    if (selectIcon == Icons.Filled.ArrowBack) {
-                        Color.Cyan
-                    } else {
-                        Color.White
-                    }, CircleShape
-                )
-                .clickable { selectIcon = Icons.Filled.ArrowBack }){ Icon(Icons.Filled.ArrowBack, "",
-                Modifier
-                    .padding(12.dp)
-                    .width(24.dp)
-                    .height(24.dp)) }
-            Spacer(modifier = Modifier.width(8.dp))
-            Box(modifier = Modifier
-                .shadow(4.dp, CircleShape)
-                .background(
-                    if (selectIcon == Icons.Filled.AddCircle) {
-                        Color.Cyan
-                    } else {
-                        Color.White
-                    }, CircleShape
-                )
-                .clickable { selectIcon = Icons.Filled.AddCircle }){ Icon(Icons.Filled.AddCircle, "",
-                Modifier
-                    .padding(12.dp)
-                    .width(24.dp)
-                    .height(24.dp)) }
-            Spacer(modifier = Modifier.width(8.dp))
-            Box(modifier = Modifier
-                .shadow(4.dp, CircleShape)
-                .background(
-                    if (selectIcon == Icons.Filled.Build) {
-                        Color.Cyan
-                    } else {
-                        Color.White
-                    }, CircleShape
-                )
-                .clickable { selectIcon = Icons.Filled.Build }){ Icon(Icons.Filled.Build, "",
-                Modifier
-                    .padding(12.dp)
-                    .width(24.dp)
-                    .height(24.dp)) }
-            Spacer(modifier = Modifier.width(8.dp))
-            Box(modifier = Modifier
-                .shadow(4.dp, CircleShape)
-                .background(
-                    if (selectIcon == Icons.Filled.Call) {
-                        Color.Cyan
-                    } else {
-                        Color.White
-                    }, CircleShape
-                )
-                .clickable { selectIcon = Icons.Filled.Call }){ Icon(Icons.Filled.Call, "",
-                Modifier
-                    .padding(12.dp)
-                    .width(24.dp)
-                    .height(24.dp)) }
-        }
-    }
-*/
     @Preview
     @Composable
     fun ComposablePreview() {
@@ -434,25 +234,4 @@ class MainActivity : ComponentActivity() {
             MainScreen()
         }
     }
-
-/*
-    Box(modifier = Modifier.shadow(4.dp, RoundedCornerShape(8.dp))
-    .background(Color.White, RoundedCornerShape(8.dp))
-    ){
-        IconButton(onClick = {isDropdownOpen = true}) {
-            Icon(selectIcon, "")
-        }
-        DropdownMenu(
-            expanded = isDropdownOpen,
-            onDismissRequest = { isDropdownOpen = false }
-        ){
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.ThumbUp}){ Icon(Icons.Filled.ThumbUp, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.AccountBox}){ Icon(Icons.Filled.AccountBox, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.Search}){ Icon(Icons.Filled.Search, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.Call}){ Icon(Icons.Filled.Call, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.Check}){ Icon(Icons.Filled.Check, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-            Box(modifier = Modifier.clickable{isDropdownOpen = false; selectIcon = Icons.Filled.ThumbUp}){ Icon(Icons.Filled.ThumbUp, "", Modifier.padding(12.dp).width(24.dp).height(24.dp)) }
-        }
-    }
-    */
 }
